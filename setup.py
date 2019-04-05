263d6123bf23954b2a3bd3d167b0440eb430aa4a
Antony Chazapis <chazapis@gmail.com>
2019-04-05 23:11:45 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='pydv',
-    version='1.0',
+    version='2.0',
     author='Antony Chazapis',
     author_email='chazapis@gmail.com',
     description='D-STAR library and utilities in Python',
